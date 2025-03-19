f2ee400a5d712568160c8f4483f3eca214c344d4
Oleksandr Mulyk <mulyk.aleks@gmail.com>
2025-03-19 09:29:25 +0100

    Add project files.

---
+++ RexCommerce.AppHost/Program.cs
@@ -0,0 +1,10 @@
+var builder = DistributedApplication.CreateBuilder(args);
+
+var apiService = builder.AddProject<Projects.RexCommerce_ApiService>("apiservice");
+
+builder.AddProject<Projects.RexCommerce_Web>("webfrontend")
+    .WithExternalHttpEndpoints()
+    .WithReference(apiService)
+    .WaitFor(apiService);
+
+builder.Build().Run();
