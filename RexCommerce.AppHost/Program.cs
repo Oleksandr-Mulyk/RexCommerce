6223af4bbab2a30363868b2a7bd65205a678a912
Oleksandr Mulyk <mulyk.aleks@gmail.com>
2025-03-24 10:14:46 +0100

    Added sql server for catalog

--- RexCommerce.AppHost/Program.cs
+++ RexCommerce.AppHost/Program.cs
@@ -1,13 +1,18 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
+var catalogSql = builder.AddSqlServer("catalog-sql")
+    .WithLifetime(ContainerLifetime.Persistent);
+
+catalogSql.AddDatabase("catalogdb");
+
 //var apiService = builder.AddProject<Projects.RexCommerce_ApiService>("apiservice");
+
+builder.AddProject<Projects.RexCommerce_CatalogGrpcService>("catalog-grpc-service")
+    .WithReference(catalogSql)
+    .WaitFor(catalogSql);
 
 builder.AddProject<Projects.RexCommerce_Web>("webfrontend")
     .WithExternalHttpEndpoints();
-    //.WithReference(apiService)
-    //.WaitFor(apiService);
-
-builder.AddProject<Projects.RexCommerce_CatalogGrpcService>("rexcommerce-cataloggrpcservice");
     //.WithReference(apiService)
     //.WaitFor(apiService);
 
